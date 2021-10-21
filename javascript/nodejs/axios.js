1a82e4168657e0d471d0ae1def30029f8e245b6d
Rei <33325105+Eilaluth@users.noreply.github.com>
2021-10-21 19:18:01 +0700

    salah

--- javascript/nodejs/axios.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('https://api.rei.my.id/api/quotes')
-  .then(function (response) {
-    console.log(response);
-})
-  .catch(function (error) {
-    console.log(error);
-})
